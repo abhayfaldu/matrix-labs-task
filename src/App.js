e789a5d31320b224c99a7e3ff0bbb7f7f7b0ea2e
abhayfaldu <abhayfaldu1922@gmail.com>
2023-09-08 16:36:31 +0530

    'added search functionality in the token address without design'
    'In this commit I have completed functionlity of the token address page and but I
    haven't designed it yet'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Sidebar from './Components/Sidebar.jsx'
-import AllRoutes from './Router/AllRoutes.jsx'
+import "./App.css";
+import Sidebar from "./Components/Sidebar.jsx";
+import AllRoutes from "./Router/AllRoutes.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-      <AllRoutes />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Sidebar />
+			<AllRoutes />
+		</div>
+	);
 }
 
 export default App;
--- src/Components/TokenAddress.jsx
+++ src/Components/TokenAddress.jsx
@@ -1,94 +1,128 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import styles from "../Styles/TokenAddress.module.css";
-
-const url =
-	"https://api.dexscreener.com/latest/dex/tokens/0x2170Ed0880ac9A755fd29B2688956BD959F933F8";
+import Search from "./Search";
 
 const TokenAddress = () => {
 	const [data, setData] = useState([]);
+	const [query, setQuery] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [isFalseQuery, setIsFalseQuery] = useState(false);
+
+	const url = `https://api.dexscreener.com/latest/dex/tokens/${query}`;
+	console.log(url);
 
 	const getData = async () => {
 		try {
-			const response = await axios(url);
-			setData(response.data.pairs);
+			if (url !== "https://api.dexscreener.com/latest/dex/tokens/") {
+				setIsLoading(true);
+				const response = await axios(url);
+				if (response?.data?.pairs === null) {
+					setIsLoading(false);
+					setIsFalseQuery(true);
+					return;
+				} else {
+					setIsFalseQuery(false);
+					setData(response?.data?.pairs);
+					setIsLoading(false);
+				}
+			}
 		} catch (error) {
+			setIsError(true);
 			throw new Error(error);
 		}
 	};
 
 	useEffect(() => {
 		getData();
-	}, []);
+	}, [query]);
 
 	console.log("data:", data);
 
 	return (
-		<div className={styles.tokenAddress__section}>
-			{data?.slice(0, 10)?.map((ele) => (
-				<div key={ele.baseToken.address} className={styles.tokenAddress__row}>
-					<div className={styles.baseInfoCard__box}>
-						<p>Base Info</p>
-						<div>
-							<p>Pair Created at</p>
-							<p>{ele.chainId}</p>
-						</div>
-						<div>
-							<p>Symbol</p>
-							<p>{ele.baseToken.symbol}</p>
-						</div>
-						<div>
-							<p>Dex ID</p>
-							<p>{ele.dexId}</p>
-						</div>
-						<div>
-							<p>Pair Address</p>
-							<p className={styles.short}>{ele.pairAddress}</p>
-						</div>
-					</div>
-					<div className={styles.baseTokenCard__box}>
-						<p>Base Token</p>
-						<div>
-							<p>Name</p>
-							<p>{ele.baseToken.name}</p>
-						</div>
-						<div>
-							<p>Symbol</p>
-							<p>{ele.baseToken.symbol}</p>
-						</div>
-						<div>
-							<p>Address</p>
-							<p className={styles.short}>{ele.baseToken.address}</p>
-						</div>
-					</div>
-					<div className={styles.quoteTokenCard__box}>
-						<p>Quote Token</p>
-						<div>
-							<p>Name</p>
-							<p>{ele.quoteToken.name}</p>
-						</div>
-						<div>
-							<p>Symbol</p>
-							<p>{ele.quoteToken.symbol}</p>
-						</div>
-						<div>
-							<p>Address</p>
-							<p className={styles.short}>{ele.quoteToken.address}</p>
-						</div>
-					</div>
-					<div className={styles.priceCard__box}>
-						<p>Price</p>
-						<div>
-							<p>Price Native</p>
-							<p>{ele.priceNative}</p>
-						</div>
-						<div>
-							<p>Price USD</p>
-							<p>{ele.priceUsd}</p>
-						</div>
-					</div>
-				</div>
-			))}
+		<div>
+			<Search query={query} onChange={(e) => setQuery(e.target.value)} />
+
+			<div className={styles.tokenAddress__data}>
+				{isLoading ? (
+					<p>Loading....</p>
+				) : isError ? (
+					<p>Something went wrong</p>
+				) : isFalseQuery ? (
+					<p>Please enter valid search query</p>
+				) : !data?.length ? (
+					<p>Please enter search query</p>
+				) : (
+					<>
+						{data?.slice(0, 10)?.map((ele) => (
+							<div key={uuidv4()} className={styles.tokenAddress__row}>
+								<div className={styles.baseInfoCard__box}>
+									<p>Base Info</p>
+									<div>
+										<p>Pair Created at</p>
+										<p>{ele?.chainId}</p>
+									</div>
+									<div>
+										<p>Symbol</p>
+										<p>{ele?.baseToken?.symbol}</p>
+									</div>
+									<div>
+										<p>Dex ID</p>
+										<p>{ele?.dexId}</p>
+									</div>
+									<div>
+										<p>Pair Address</p>
+										<p className={styles.short}>{ele?.pairAddress}</p>
+									</div>
+								</div>
+								<div className={styles.baseTokenCard__box}>
+									<p>Base Token</p>
+									<div>
+										<p>Name</p>
+										<p>{ele?.baseToken?.name}</p>
+									</div>
+									<div>
+										<p>Symbol</p>
+										<p>{ele?.baseToken?.symbol}</p>
+									</div>
+									<div>
+										<p>Address</p>
+										<p className={styles.short}>{ele?.baseToken?.address}</p>
+									</div>
+								</div>
+								<div className={styles.quoteTokenCard__box}>
+									<p>Quote Token</p>
+									<div>
+										<p>Name</p>
+										<p>{ele?.quoteToken.name}</p>
+									</div>
+									<div>
+										<p>Symbol</p>
+										<p>{ele?.quoteToken.symbol}</p>
+									</div>
+									<div>
+										<p>Address</p>
+										<p className={styles.short}>{ele?.quoteToken.address}</p>
+									</div>
+								</div>
+								<div className={styles.priceCard__box}>
+									<p>Price</p>
+									<div>
+										<p>Price Native</p>
+										<p>{ele?.priceNative}</p>
+									</div>
+									<div>
+										<p>Price USD</p>
+										<p>{ele?.priceUsd}</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</>
+				)}
+			</div>
 		</div>
 	);
 };
